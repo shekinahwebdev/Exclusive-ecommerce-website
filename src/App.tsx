d8af4da7b1111755702865f5719fb5cbaa01b571
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-06-22 23:56:43 +0000

    Add SignUpPage component and integrate it into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
   return (
     <div className="">
       <HomePage />
       <LoginPage />
+      <SignUpPage />
     </div>
   );
 };
---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./HomePage";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default SignUpPage;
